cd3842f259d5dbe4b4c58b9d78f545b8a8074724
qiyuanchen <chenqiyuan1012@foxmail.com>
2023-10-18 19:57:04 +0800

    update index

---
+++ README.md
@@ -0,0 +1,6 @@
+# 睿医网信办
+
+## 依赖
+
+后端：python+fastapi
+前端：vue+element-plus
--- client/src/App.vue
+++ client/src/App.vue
@@ -5,6 +5,7 @@
       <div class="site-title">
         睿医网信办
       </div>
+      <p>这个项目由<a href="https://qiyuan-chen.github.io/">陈启源</a>开发。感谢杜邦的支持。代码已经在<a href="https://github.com/qiyuan-chen/RealDoctorMonitor">Github</a>开源，欢迎大家提出建议和改进！</p>
       <el-menu mode="horizontal">
         <el-menu-item index="1" @click="activeComponent = 'gpu-monitor'">各服务器GPU占用情况</el-menu-item>
         <el-menu-item index="2" @click="activeComponent = 'sort-by-usage'">按使用量排序</el-menu-item>
@@ -19,7 +20,7 @@
     </el-main>
 
     <!-- Footer -->
-    <el-footer class="site-footer">© 2023 由<a href="https://qiyuan-chen.github.io/">陈启源</a>开发。感谢杜邦支持。</el-footer>
+    <el-footer class="site-footer">© 2023 浙大睿医</el-footer>
   </div>
 </template>
 
